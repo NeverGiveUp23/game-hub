25e6c682b08399e252ab9c79f6d20da74d1579af
felix <felixvjr0@gmail.com>
2023-10-18 16:35:07 -0400

    Fetch games with reactQuery, Caching

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -2,6 +2,10 @@ import axios from "axios";
 
 const API_KEY = 'b7bbaf190c6846e09febbbdb8c275dd6'
 
+export interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
 
 export default axios.create({
   baseURL: 'https://api.rawg.io/api',
